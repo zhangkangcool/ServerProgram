884eb1122627b709f37a85d2b35c946a36f3a8a9
Ken <ken@ken-VirtualBox.(none)>
2016-09-10 20:22:08 +0800

    define class process and class processpool

--- ProcessPool/processpool.h
+++ ProcessPool/processpool.h
@@ -17,6 +17,53 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 
+class process {
+public:
+	process(): m_pid(-1){}
 
+public:
+	pid_t m_pid;
+	int m_pipefd[2];
+};
+
+template<typename T>
+class processpool {
+private:
+	processpool(int listenfd, int process_number = 8);
+	
+public:
+	static process<T>* create(int listenfd, int process_number = 8) {
+		if (!m_instance) {
+			m_instance = new processpool<T>(listenfd, process_number);
+		}
+
+		return m_instance;
+	}
+
+	~processpool() {
+		delete[] m_sub_process;
+	}
+
+	void run();
+
+private:
+	void setup_sig_pipe();
+	void run_parent();
+	void run_child();
+
+private:
+	static const int MAX_PROCESS_NUMBER = 16;
+	static const int USER_PER_PROCESS = 65536;
+	static const int MAX_EVENT_NUMBER = 10000;
+	int m_process_number;
+	int m_idx;
+	int m_epollfd;
+	int m_listenfd;
+	int m_stop;
+
+	process* m_sub_process;
+	static processpool<T>* m_instance;
+};
 
 #endif
+
